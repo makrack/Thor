a74f6ccb8c2d5b91757df5f1cb4963956f226386
makruiz@gmail.com <Makmv540>
2019-11-22 15:07:20 -0600

    primer

---
+++ JsFunciones2.js
@@ -0,0 +1,16 @@
+function testAnim(x) {
+    $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+};
+
+
+$(document).ready(function() {
+
+$('#myModal').on('show.bs.modal', function (e) {
+  var anim = "wobble";
+      testAnim(anim);
+})
+$('#myModal').on('hide.bs.modal', function (e) {
+  var anim = "wobble";
+      testAnim(anim);
+})
+});
